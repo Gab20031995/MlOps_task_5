6bb7a0662a645476f7e876538fe44ce7af73a69d
Gab30031995 <luizclzm@gmail.com>
2024-03-11 01:12:05 -0600

    Cambios

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-import uuid
-
 from starlette import status
+from pydantic import BaseModel
+from typing import Dict, List
 
-#Registro de Usuarios:
 app = FastAPI(
     title="API's Task 5 MLops",
     version="0.0.1"
 )
 
+# Simulación de almacenamiento de datos en memoria
+db_users = {}
+db_tasks = {}
+
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class Task(BaseModel):
+    title: str
+    description: str
+    status: str
+
+#Registro de Usuarios:
 @app.post("/api/v1/register/")
-async def create_user(username: str, name: str,password: str):
+async def create_user(user: User):
+    if user.username in db_users:
+        raise HTTPException(status_code=400, detail="El usuario ya existe")
+    db_users[user.username] = user
     return {
-        "username": username,
-        "email": name,
-        "Password": password,
+        "username": user.username,
+        "email": user.email,
+        "Password": user.password,
         "message": "The user has been created successfully",
         "status_Code": 201
     }
 
 #Obtener Datos de Usuario:
-
-@app.get("/api/v1/user/{user_id}")
-async def get_user(user_id: str):
-    # Aquí iría la lógica para obtener los datos del usuario con el ID proporcionado
-    # Por simplicidad, devolvemos un usuario de ejemplo si el ID es "1"
-    if user_id == "1":
-        return {
-            "user_id": user_id,
-            "username": "example_user",
-            "email": "example@example.com"
-        }
+@app.get("/api/v1/user/{username}")
+async def get_user(username: str):
+    if username in db_users:
+        user = db_users[username]
+        return user
     else:
         return JSONResponse(
             content={"message": "User not found"},
             status_code=status.HTTP_404_NOT_FOUND
         )
+
